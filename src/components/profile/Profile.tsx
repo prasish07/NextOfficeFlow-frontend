39dbeb70a1ba8922afe7a07d09ee4b4a77883c6b
prasish07 <prasishshrestha0099@gmail.com>
2024-05-01 21:26:10 +0545

    Add leave information of employee in profile page

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -29,44 +29,64 @@ const Profile = ({ userId }: { userId: string }) => {
 	return (
 		<div className="profile__details">
 			<h2>Job Information</h2>
-			<p>
-				<span>Position :</span>
-				{details.position}
-			</p>
-			<p>
-				<span>Department:</span>
-				{details.department}{" "}
-			</p>
-			<p>
-				<span>Team:</span>
-				{details.team}
-			</p>
-			<p>
-				<span>Supervisor:</span>
-				{details.manager}
-			</p>
-			<p>
-				<span>Start Date:</span>
-				{dateWordFormatter(details.startDate)}
-			</p>
-			<p>
-				<span>End Date:</span>
-				{dateWordFormatter(details.endDate)}
-			</p>
-			<p>
-				<span>Salary:</span>
-				{details.salary}
-			</p>
-			<p>
-				<span>Salary Type:</span>Monthly
-			</p>
-			<p>
-				<span>Status: </span>
-				{details.status}
-			</p>
-			<p>
-				<span>Working Hours:</span>Mon - Fri, {details.from} to {details.to}
-			</p>
+			<div className="profile__details--element">
+				<div>
+					<p>
+						<span>Position :</span>
+						{details.position}
+					</p>
+					<p>
+						<span>Department:</span>
+						{details.department}{" "}
+					</p>
+					<p>
+						<span>Team:</span>
+						{details.team}
+					</p>
+					<p>
+						<span>Supervisor:</span>
+						{details.manager}
+					</p>
+					<p>
+						<span>Start Date:</span>
+						{dateWordFormatter(details.startDate)}
+					</p>
+					<p>
+						<span>End Date:</span>
+						{dateWordFormatter(details.endDate)}
+					</p>
+					<p>
+						<span>Salary:</span>
+						{details.salary}
+					</p>
+					<p>
+						<span>Salary Type:</span>Monthly
+					</p>
+					<p>
+						<span>Status: </span>
+						{details.status}
+					</p>
+					<p>
+						<span>Working Hours:</span>Mon - Fri, {details.from} to {details.to}
+					</p>
+				</div>
+
+				<div>
+					<p>
+						<span>Total paid leaves:</span> {details.availableLeaves}
+					</p>
+					<p>
+						<span>Total paid leaves taken:</span> {details.totalPaidLeaveTaken}
+					</p>
+					<p>
+						<span>Total unpaid leaves taken:</span>{" "}
+						{details.totalUnpaidLeaveTaken}
+					</p>
+					<p>
+						<span>Total leaves taken:</span> {details.leavesTaken}
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 };
